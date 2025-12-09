380b2f4a1830f130d4261a4ba22081e5363d6f5f
Diego Ramos <rfdiego@icloud.com>
2025-12-09 14:52:10 -0400

    make it easy to test on web editor

--- src/app/drawings/track-mouse-with-ball.tsx
+++ src/app/drawings/track-mouse-with-ball.tsx
@@ -3,18 +3,25 @@
 import type p5 from "p5";
 import { P5Canvas } from "@/components/p5";
 
-function draw(p: p5) {
-	p.background("tomato");
+function draw({
+	background,
+	mouseIsPressed,
+	fill,
+	circle,
+	mouseX,
+	mouseY,
+}: p5) {
+	background("tomato");
 
-	if (p.mouseIsPressed === true) {
+	if (mouseIsPressed === true) {
 		//when mouse button is pressed, circles turn black
-		p.fill(0);
+		fill(0);
 	} else {
-		p.fill(255);
+		fill(255);
 	}
 
 	//white circles drawn at mouse position
-	p.circle(p.mouseX, p.mouseY, 100);
+	circle(mouseX, mouseY, 100);
 }
 
 export function TrackMouseWithBall() {
