f483fcf74bb5b0b7b894e56e77de9dd3b9e632c3
Diego Ramos <rfdiego@icloud.com>
2025-12-10 00:03:28 -0400

    make easier to understand

--- src/app/tiles/tiles.tsx
+++ src/app/tiles/tiles.tsx
@@ -8,20 +8,25 @@ let rows = 0;
 let blocks: ReturnType<typeof Block>[][] = [];
 const mouseDistance = 30;
 
-function Block(x: number, y: number, size: number) {
+function Block(i: number, j: number, size: number) {
+	// The position of the block is calculated based on its grid indices
+	// and the size of each block to center it within its grid cell
+	// Example: for i=0, j=0 and size=50, x=25, y=25
+	// for i=1, j=0 and size=50, x=75, y=25, etc.
+	const x = i * size + size / 2;
+	const y = j * size + size / 2;
+
 	return {
-		x,
-		y,
 		angle: 0,
 		display(p: p5) {
 			p.push();
-			p.translate(this.x, this.y);
+			p.translate(x, y);
 			p.rotate(this.angle);
 			p.rect(0, 0, size, size);
 			p.pop();
 		},
 		move(p: p5) {
-			const distance = p.dist(p.mouseX, p.mouseY, this.x, this.y);
+			const distance = p.dist(p.mouseX, p.mouseY, x, y);
 			if (p.pmouseX - p.mouseX !== 0 || p.pmouseY - p.mouseY !== 0) {
 				if (distance < mouseDistance) {
 					this.angle += 1;
@@ -44,9 +49,7 @@ function setup(p: p5) {
 	rows = p.height / size;
 
 	blocks = Array.from({ length: colums }, (_, i) =>
-		Array.from({ length: rows }, (_, j) =>
-			Block(i * size + size / 2, j * size + size / 2, size),
-		),
+		Array.from({ length: rows }, (_, j) => Block(i, j, size)),
 	);
 }
 
